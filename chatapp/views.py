9bf5db3ddc3744c2e44f7fbee20e0aae373ec1bc
Leonte <leonte_petru@yahoo.com>
2022-11-27 21:01:47 +0200

    workingLogin1

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
+
+def rooms(request):
+    return render(request,'chatRoom.html')
+
+def checkCredentials(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user= authenticate (username=username, password = password)
+
+    if user is not None:
+        return  redirect('room/')
