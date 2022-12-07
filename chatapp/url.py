37da4a57ffb5d789b9ce085d52e5f6aa07951e40
Leonte <leonte_petru@yahoo.com>
2022-12-07 22:14:57 +0200

    Working chat

--- chatapp/url.py
+++ chatapp/url.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns=[
     path('', views.login, name='loginHome'),
     path('login/',views.login,name='login'),
-    #path('room/<str:room>/<str:user>', views.room, name='room'),
+    path('room/<str:room>/<str:username>', views.room, name='room'),
     path('checkCredentials',views.checkCredentials,name='checkCredentials'),
     path('checkRoom',views.checkRoom,name='checkRoom'),
     path('getUserRooms/<str:user>/', views.getUserRooms, name='getUserRooms'),
-    path('roomList/<str:user>/', views.roomList, name='roomList')
+    path('roomList/<str:user>/', views.roomList, name='roomList'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages')
 ]
--- chatapp/views.py
+++ chatapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
-from chatapp.models import Room, ChatRoomParticipant
+from chatapp.models import Room, ChatRoomParticipant, Message
 from django.http import HttpResponse, JsonResponse
 
 
@@ -15,9 +15,9 @@ def roomList(request, user):
         'username': user,
     })
 
-def room(request, room, username):
-    room_details = Room.objects.get(name=room)
-    return render(request, 'room.html', {
+def room(request,room,username):
+    room_details = Room.objects.get(roomName=room)
+    return render(request, 'chatRoom.html', {
         'username': username,
         'room': room,
         'room_details': room_details
@@ -33,18 +33,33 @@ def checkCredentials(request):
         return  redirect('/roomList/'+username)
 
 def checkRoom(request):
-    room = request.POST['roomName']
+    roomName = request.POST['roomName']
     username = request.POST['username']
 
-    if Room.objects.filter(name=room).exists():
-        return redirect('/room'+room+'/?username='+username)
+    if Room.objects.filter(roomName=roomName).exists():
+        return redirect('/room/' + roomName + '/' + username)
     else:
-        new_room = Room.objects.create(name=room)
+        new_room = Room.objects.create(roomName=roomName)
         new_room.save()
-        return redirect('/room/?room='+room+'/?username='+username)
+        return redirect('/room/' + roomName + '/' + username)
 
 def getUserRooms(request, user):
     user_details = User.objects.get(username=user)
     rooms = ChatRoomParticipant.objects.filter(user=user_details.username)
     return JsonResponse({"rooms":list(rooms.values())})
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(text=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('Message sent successfully')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(roomName=room)
+
+    messages = Message.objects
+    return JsonResponse({"messages":list(messages.values())})
 
